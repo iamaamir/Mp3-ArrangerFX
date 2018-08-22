4e6d33a2378d33fa465fa633fcc0163741323c07
Aamir khan <aamirkhan180@gmail.com>
2018-08-22 23:32:36 +0530

    more refactoring

--- src/mp3a/arrangerfx/FXMLDocumentController.java
+++ src/mp3a/arrangerfx/FXMLDocumentController.java
@@ -3,24 +3,24 @@ package mp3a.arrangerfx;
 import java.io.File;
 import java.net.URL;
 import java.time.Year;
+import javafx.fxml.FXML;
+import javafx.util.Duration;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Button;
 import javafx.scene.control.Tooltip;
 import javafx.stage.DirectoryChooser;
-import javafx.util.Duration;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.collections.FXCollections;
+import javafx.scene.control.ProgressBar;
+import javafx.collections.ObservableList;
+import javafx.beans.value.ObservableValue;
 import static mp3a.arrangerfx.Util.getResource;
 
 /**
@@ -41,8 +41,10 @@ public class FXMLDocumentController implements Initializable {
     private Button selectFolder;
     @FXML
     private ProgressBar progressBar;
+    
     private final String DEFAULT_MUSIC_DIRECTORY;
     private DirectoryChooser dirChooser;
+    private Timeline dummyAnimation;
 
     public FXMLDocumentController() {
         this.DEFAULT_MUSIC_DIRECTORY = System.getProperty("user.home") + File.separatorChar + "Music";
@@ -60,7 +62,7 @@ public class FXMLDocumentController implements Initializable {
         dirChooser.setTitle(getResource("CHOOSER"));
         dirChooser.setInitialDirectory(new File(DEFAULT_MUSIC_DIRECTORY));
 
-        Timeline task = new Timeline(
+        dummyAnimation = new Timeline(
                 new KeyFrame(
                         Duration.ZERO,
                         new KeyValue(progressBar.progressProperty(), 0)
@@ -81,15 +83,15 @@ public class FXMLDocumentController implements Initializable {
         choice.getSelectionModel().selectFirst();
 
         path.setText(DEFAULT_MUSIC_DIRECTORY);
+        path.setEditable(false);
 
         copyrightLabel.visibleProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
-            if (newValue == true) {
-                System.out.println(newValue);
-                progressBar.setVisible(false);
-            } else {
-                progressBar.setVisible(true);
-                task.playFromStart();
-            }
+//            if (newValue == true) {
+//                progressBar.setVisible(false);
+//            } else {
+//                progressBar.setVisible(true);
+//            }
+              progressBar.setVisible(!newValue);
         });
     }
 
@@ -103,6 +105,11 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     public void goButtonClick(ActionEvent e) {
         copyrightLabel.setVisible(false);
+        dummyAnimation.playFromStart();
+        dummyAnimation.setOnFinished((event) -> {
+            copyrightLabel.setVisible(true);
+        });
+        
     }
 
     private String currentYear() {
